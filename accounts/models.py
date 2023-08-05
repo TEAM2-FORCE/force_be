8ac79be724ade3e4df397347f66633a46530b20c
oro7l <xz305@naver.com>
2023-08-05 21:44:22 +0900

    fix: email max_length 수정, serializer 삭제

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ class User(AbstractUser) :
     # name = models.CharField(max_length = 10, blank = True, null = True)
     # username = models.CharField(max_length = 10, unique = True, blank = True, null = True)
     # social_id = models.CharField(max_length = 50, blank = True, null = True)
-    email = models.CharField(max_length = 254, unique = True, blank = True, null = True)
+    email = models.CharField(max_length = 50, unique = True, blank = True, null = True)
 
     # USERNAME_FIELD = 'email'
     # REQUIRED_FIELDS = [
