eb476e6e3ec91c1df2fdf5c93bad75d9416314f0
devrokket <rokany@cau.ac.kr>
2023-08-06 00:32:33 +0900

    fix: control char 삭제

--- products/views.py
+++ products/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 
-# Create your views here.
-
 def hello_world(request):
     if request.method == "GET":
         return JsonResponse({
