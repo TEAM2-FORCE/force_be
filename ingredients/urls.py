d7e4e365171d241b9de42f0300c62e5449c07ad9
oro7l <xz305@naver.com>
2023-08-08 22:03:46 +0900

    feat: 성분 북마크 기능 틀 구현

--- ingredients/urls.py
+++ ingredients/urls.py
@@ -5,4 +5,7 @@ from .models import *
 urlpatterns = [
     path('', IgdList.as_view(), name = 'ingredients_list'),
     path('<int:id>/', IgdDetail.as_view(), name = 'ingredient_detail'),
+    
+    path('<int:igd_id>/bm/', IgdBm.as_view(), name = 'ingredient_bookmark'),
+    path('bm/', IgdBm.as_view(), name = 'ingredients_bookmark_list'),
 ]
--- ingredients/views.py
+++ ingredients/views.py
@@ -6,8 +6,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
 
-from .models import Ingredient
-from .serializers import IgdSerializer
+from .models import *
+from .serializers import *
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
@@ -35,7 +35,38 @@ class IgdDetail(APIView):
 
     # 개별 성분 조회
     def get(self, request, id):
-    
         igd = get_object_or_404(Ingredient, igd_id = id)
         serializer = IgdSerializer(igd)
         return Response(serializer.data)
+    
+        
+
+class IgdBm(APIView):
+
+    # 전체 성분 조회
+    def get(self, request, format = None):
+        bm_igds = Bookmark.objects.all()
+        serializer = IgdBmSerializer(bm_igds, many = True) #다수 객체 이용
+        return Response(serializer.data)
+
+    # 성분 저장
+    def post(self, request, igd_id, foramt = None):
+        try :
+            igd = Ingredient.objects.get(igd_id = igd_id)
+        except Ingredient.DoesNotExist:
+            return Response({"error" : "Ingredients not found"}, status = status.HTTP_404_NOT_FOUND)
+
+        data = {
+            "user" : request.user.id, #if request.user.is_authenticated else None,
+            "igd" : igd.igd_id,
+        }
+
+        serializer = IgdBmSerializer(data = data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        else : 
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+    #def delete(self, request, id):
