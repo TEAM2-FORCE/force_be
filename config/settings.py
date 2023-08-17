253f588ca68aec23b9935e42440fe54a54d9b315
Ubuntu <ubuntu@ip-172-31-1-41.ap-northeast-2.compute.internal>
2023-08-17 12:54:13 +0000

    fix: settings.py 수정

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # 모든 IP 허용
 ALLOWED_HOSTS = ['*']
