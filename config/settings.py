bb181a61d5d04353fa2d585a0308e785c2b7be41
devrokket <rokany@cau.ac.kr>
2023-08-17 19:48:24 +0900

    fix:2

--- config/settings.py
+++ config/settings.py
@@ -5,10 +5,6 @@ import os, json
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# nginx 정적파일 읽기
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '_static')
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -171,6 +167,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
